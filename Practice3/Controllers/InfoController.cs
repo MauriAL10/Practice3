a4bad1bafd1a439920ef8b795cde404e3bf1576b
MauriAL10 <mau.27.02@gmail.com>
2021-04-29 16:47:14 -0400

    Se creó el nuevo endpoint y se añadio el metodo GetInfo

---
+++ Practice3/Controllers/InfoController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practice3.Controllers
+{
+    [ApiController]
+    [Route("/api/info")]
+    public class InfoController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public InfoController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public string GetInfo()
+        {
+            string projectTitle = _config.GetSection("ProjectTitle").Value;
+            string environmentName = _config.GetSection("EnvironmentName").Value;
+
+            string dbConnection = _config.GetConnectionString("Database");
+
+            Console.Out.WriteLine($"Connection String: {dbConnection}");
+
+            return $"Project Title: {projectTitle} \n" +
+                $"Environment Name: {environmentName}";
+        }
+    }
+}
---
+++ Practice3/Info.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Practice3
+{
+    public class Info
+    {
+        public String Title { get; set; }
+    }
+}
